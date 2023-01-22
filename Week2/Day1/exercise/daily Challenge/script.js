3fad55f3eab53cf6e9b683eb10704de288944639
elieaidan <elieaidan5@gmail.com>
2023-01-22 16:32:41 +0200

    daily

---
+++ Week2/Day1/exercise/daily Challenge/script.js
@@ -0,0 +1,46 @@
+
+// Daily challenge
+// Exercise 1
+
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.splice(0, 1);
+
+fruits.sort();
+
+// console.log(fruits);
+
+fruits.push("Kiwi");
+
+fruits.shift("Apples");
+
+// console.log(fruits);
+
+fruits.reverse();
+
+// console.log(fruits);
+
+// ["Kiwi", "Oranges", "Blueberries"]
+
+// Exercise 2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+const red = moreFruits[1];
+
+console.log(red);
+
+const rond = red [1];
+
+console.log(rond);
+
+
+
+
+
+
+
+
+
+
