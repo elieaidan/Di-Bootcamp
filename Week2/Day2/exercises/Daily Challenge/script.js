2ea311f1e060cb90fbdc6601f21f6a5378397389
elieaidan <elieaidan5@gmail.com>
2023-01-23 15:43:08 +0200

    ajout

---
+++ Week2/Day2/exercises/Daily Challenge/script.js
@@ -0,0 +1,31 @@
+
+// DailyChallenge
+
+
+const sentence = "The movie is not that bad, I like it";
+
+const wordNot = sentence.indexOf("not");
+
+const wordBad = sentence.indexOf("bad");
+
+console.log(wordNot);
+
+console.log(wordBad);
+
+
+
+ if (wordNot < wordBad)  {
+    console.log(`The movie is good, I like it`);
+
+
+ } else {
+
+
+console.log(`The movie is not that bad, I like it`);
+
+
+ }
+
+
+
+
