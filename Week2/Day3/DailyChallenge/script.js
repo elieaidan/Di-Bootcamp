265d131ef45b29ed4fc1e44e667186517781f593
elieaidan <elieaidan5@gmail.com>
2023-01-24 17:13:21 +0200

    dc

---
+++ Week2/Day3/DailyChallenge/script.js
@@ -0,0 +1,17 @@
+// DailyChallenge
+
+for (let i = 0; i <= 0; i++) {
+    console.log("*");
+    for (let j = 0; j <= 0; j++) {
+      console.log("* *");
+    }
+    for (let e = 0; e <= 0; e++) {
+        console.log("* * *");
+      }
+      for (let r = 0; r <= 0; r++) {
+        console.log("* * * *");
+      }
+      for (let s = 0; s <= 0; s++) {
+        console.log("* * * * *");
+      }
+  }
