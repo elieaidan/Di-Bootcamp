a7aad9eb8fd9fc08f8c518db114b2d3415fccfa9
elieaidan <elieaidan5@gmail.com>
2023-02-14 15:52:18 +0200

    dd

---
+++ Week5/Day3/dailychallenge/dc1.py
@@ -0,0 +1,52 @@
+import math
+
+class Circle:
+    def __init__(self,radius,str):
+        self.str=str
+        self.radius = radius if str=="Radius" else radius/2
+    def circle_area(self):
+        return math.pi*self.radius**2
+    def __add__(self, other):
+        return self.radius+other.radius
+    def __gt__(self, other):
+        return self.radius > other.radius
+    def sort(self,other):
+        d=[]
+        d.append(int(self.radius))
+        d.append(int(other.radius))
+        d=sorted(d)
+        return d
+
+circle1 = Circle(5,"Radius")
+circle2= Circle(8,"Diameter")
+print(circle1.circle_area(),circle2.circle_area())
+d=circle1+circle2
+print(d)
+print(circle1>circle2)
+print(circle1.sort(circle2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
