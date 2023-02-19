c2f89b4341f79890ef80b7349e55b34b526fbe11
elieaidan <elieaidan5@gmail.com>
2023-02-19 13:05:26 +0200

    eso

---
+++ Week6/Day1/exo1.sql
@@ -0,0 +1,12 @@
+
+select * from public_data;
+select * from customers;
+
+select * from public_data where price > 80;
+select * from public_data where price < 300;
+select * from customers where last_name = 'Smith';
+select * from customers where last_name = 'Jones';
+select * from customers where first_name != 'Scott';
+
+
+
